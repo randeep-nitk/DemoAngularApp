9859bbe39da6d5d1a60139a8a4750c753a8879de
Randeep Singh <ransingh@ciena.com>
2017-08-02 20:42:33 +0530

    Controllers , showing repeated information etc.:wq

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -7,9 +7,36 @@ eventsApp.controller('EventController',
       $scope.event = {
         name: 'Angular Boot Camp',
         date: '1/1/2013',
-        time: '10:30 am'        
+        time: '10:30 am',
+        location: {
+            address: 'Google Headquarter',
+            city:'Moutain View',
+            province: 'CA',
+        },
+        imageUrl: '/img/angularjs-logo.png',
+        sessions:[
+          {
+            name:'Directives Masterclass',
+            creatorName:'Bob Smith',
+            duration:'1 hr',
+            level:'Advanced',
+            abstract:'In this session you will learn ins and outs of Directives'
+          },
+          {
+            name:'Scopes for fun and profit',
+            creatorName:'John Doe',
+            duration:'30 min',
+            level:'Introductory',
+            abstract:'This session will take a closed look at scopes'
+          },
+          {
+            name:'Well behaved Controllers',
+            creatorName:'Bob Smith',
+            duration:'2 hrs',
+            level:'Intermediate',
+            abstract:'Controllers are the beginning of everything Angular does'
+          }
+        ]
       }
   }
-
-
 );
